b5a2f6c529fc16434bc353a459803c2330a6da94
万里涛 <wanlitao_3055601@outlook.com>
2022-07-29 19:15:20 +0800

    add resolve base path by request forward header

--- src/FCP.Swashbuckle.AspNetCore.Swagger/Extensions/SwaggerOptionsExtensions.cs
+++ src/FCP.Swashbuckle.AspNetCore.Swagger/Extensions/SwaggerOptionsExtensions.cs
@@ -9,12 +9,26 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class SwaggerOptionsExtensions
     {
+        internal const string HttpHeader_Forward_Proto = "X-Forwarded-Proto";
+        internal const string HttpHeader_Forward_Host = "X-Forwarded-Host";
+        internal const string HttpHeader_Forward_Prefix = "X-Forwarded-Prefix";
+
         public static SwaggerOptions ResolveBasePathByRequestReferer(this SwaggerOptions options)
         {
             if (options == null)
                 throw new ArgumentNullException(nameof(options));
 
             options.PreSerializeFilters.Add(BuildBasePathFilterByCheckRequestReferer());
+
+            return options;
+        }
+
+        public static SwaggerOptions ResolveBasePathByRequestForward(this SwaggerOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            options.PreSerializeFilters.Add(BuildBasePathFilterByCheckRequestForward());
 
             return options;
         }
@@ -36,6 +50,27 @@ namespace Microsoft.AspNetCore.Builder
 
                 swaggerDoc.Servers = new List<OpenApiServer> {
                     new OpenApiServer { Url = $"{referer.Scheme}://{referer.Host}:{referer.Port}{refererPrefixPath}" }
+                };
+            };
+        }
+
+        private static Action<OpenApiDocument, HttpRequest> BuildBasePathFilterByCheckRequestForward()
+        {
+            return (swaggerDoc, httpReq) =>
+            {
+                var forwardHost = httpReq.Headers[HttpHeader_Forward_Host].ToString();
+                if (string.IsNullOrWhiteSpace(forwardHost))
+                    return;
+                
+                var docHost = httpReq.Headers[HeaderNames.Host].ToString();
+                if (string.Compare(forwardHost, docHost, true) == 0)
+                    return;
+
+                var forwardProto = httpReq.Headers[HttpHeader_Forward_Proto].ToString();
+                var forwardPrefix = httpReq.Headers[HttpHeader_Forward_Prefix].ToString();
+
+                swaggerDoc.Servers = new List<OpenApiServer> {
+                    new OpenApiServer { Url = $"{forwardProto}://{forwardHost}{forwardPrefix}" }
                 };
             };
         }
